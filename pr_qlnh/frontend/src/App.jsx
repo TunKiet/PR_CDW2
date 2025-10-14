92e8cacb962231c119b0e40afa23166623eed352
xuantrieu64 <tvxt12@gmail.com>
2025-10-14 09:02:08 +0700

    trieu commit function inventory

--- pr_qlnh/frontend/src/App.jsx
+++ pr_qlnh/frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Dashboard from './components/Dashboard/Dashboard'
+import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Dashboard/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+      </Routes>
+    </Router>
   )
 }
 
