d5c6996afed1fed983a39a6f94032e67757a2565
xuantrieu64 <tvxt12@gmail.com>
2025-10-17 10:28:13 +0700

    trieu commit manager review

--- pr_qlnh/frontend/src/App.jsx
+++ pr_qlnh/frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './App.css'
-import Dashboard from './components/Dashboard/Dashboard'
 import Sidebar from './components/Sidebar/Sidebar'
 import Header from './components/Header/Header'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Review from './components/Review/Review'
+import ReviewModerator from './components/ReviewModerator/ReviewModerator'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<ReviewModerator />} />
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/review" element={<Review />} />
       </Routes>
