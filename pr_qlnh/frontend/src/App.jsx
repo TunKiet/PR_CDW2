2bbd7bc0bce79bace0d067fad27e624fdd3dae5f
tienthanh47 <2023163ntto@gmail.com>
2025-10-18 07:34:27 +0700

    h

--- pr_qlnh/frontend/src/App.jsx
+++ pr_qlnh/frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import LoginPage from './components/LoginPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import TableManagementPage from "./pages/TableManagement";
+//import About from "./components/About";
 
 function App() {
-  return <LoginPage />
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/quanlyban" element={<TableManagementPage />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+  }
 
-export default App
+export default App;
