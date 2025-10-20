d118cf0adc014f8f96b72b38fe181141abd579ba
xuantrieu64 <tvxt12@gmail.com>
2025-10-20 14:04:48 +0700

    trieu commit

--- pr_qlnh/frontend/src/components/Inventory/IngredientAlert.jsx
+++ pr_qlnh/frontend/src/components/Inventory/IngredientAlert.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import Button from '@mui/material/Button';
+import { RiFileList2Line } from "react-icons/ri";
 const IngredientAlert = () => {
     return (
         <>
             <div className="boxIngredient-alert">
-                <div className="boxIngredient-alert-head">
+                <div className="boxIngredient-alert-head flex my-2">
                     <div className="alert-title ms-2 text-red-500 font-bold">
                         <h5>Nguyên liệu sắp hết hàng</h5>
+                    </div>
+                    <div className="boxIngredient-button ms-auto">
+                        <div className="boxIngredient-button-name me-3">
+                            <Button variant='contained' color='primary'><RiFileList2Line size={20} />Tạo đơn nhập hàng</Button>
+                        </div>
                     </div>
                 </div>
                 <div className="boxIngredient-table">
@@ -15,17 +21,18 @@ const IngredientAlert = () => {
                             <tr>
                                 <th className='px-4 py-2 text-center border-b'>Mã NL</th>
                                 <th className='px-4 py-2 text-center border-b'>Tên NL</th>
+                                <th className='px-4 py-2 text-center border-b'>Danh mục</th>
                                 <th className='px-4 py-2 text-center border-b'>Tồn kho</th>
                                 <th className='px-4 py-2 text-center border-b'>Ngưỡng</th>
                                 <th className='px-4 py-2 text-center border-b'>Đơn vị</th>
                                 <th className='px-4 py-2 text-center border-b'>Tiêu thụ</th>
-                                <th className='px-4 py-2 text-center border-b'>Thời gian</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr className='border border-b-2'>
                                 <td className='px-4 py-2 text-center border-b'>1</td>
                                 <td className='px-4 py-2 text-center border-b'>Gà</td>
+                                <td className='px-4 py-2 text-center border-b'>Thịt</td>
                                 <td className='px-4 py-2 text-center border-b'>10</td>
                                 <td className='px-4 py-2 text-center border-b'>15</td>
                                 <td className='px-4 py-2 text-center border-b'>Con</td>
@@ -43,7 +50,48 @@ const IngredientAlert = () => {
                                 </td>
                             </tr>
 
-                            
+                            <tr className='border border-b-2'>
+                                <td className='px-4 py-2 text-center border-b'>1</td>
+                                <td className='px-4 py-2 text-center border-b'>Gà</td>
+                                <td className='px-4 py-2 text-center border-b'>Thịt</td>
+                                <td className='px-4 py-2 text-center border-b'>10</td>
+                                <td className='px-4 py-2 text-center border-b'>15</td>
+                                <td className='px-4 py-2 text-center border-b'>Con</td>
+                                <td className="px-4 py-2 border-b">
+                                    <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
+                                        <progress
+                                            max={100}
+                                            value={90}
+                                            className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
+                                        ></progress>
+                                        <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
+                                            90%
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr className='border border-b-2'>
+                                <td className='px-4 py-2 text-center border-b'>1</td>
+                                <td className='px-4 py-2 text-center border-b'>Gà</td>
+                                <td className='px-4 py-2 text-center border-b'>Thịt</td>
+                                <td className='px-4 py-2 text-center border-b'>10</td>
+                                <td className='px-4 py-2 text-center border-b'>15</td>
+                                <td className='px-4 py-2 text-center border-b'>Con</td>
+                                <td className="px-4 py-2 border-b">
+                                    <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
+                                        <progress
+                                            max={100}
+                                            value={90}
+                                            className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
+                                        ></progress>
+                                        <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
+                                            90%
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+
                         </tbody>
                     </table>
                 </div>
