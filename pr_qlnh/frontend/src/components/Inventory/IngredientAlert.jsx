3c21eb8fdd13b4e413e66378fce0af40dd366947
xuantrieu64 <tvxt12@gmail.com>
2025-10-21 07:35:29 +0700

    trieu commit

--- pr_qlnh/frontend/src/components/Inventory/IngredientAlert.jsx
+++ pr_qlnh/frontend/src/components/Inventory/IngredientAlert.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import { RiFileList2Line } from "react-icons/ri";
-import Dialog from '@mui/material/Dialog'; 
+import Dialog from '@mui/material/Dialog';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 const IngredientAlert = () => {
+    const [checked, setChecked] = useState([true, false]);
+
+    const handleChange1 = (event) => {
+        setChecked([event.target.checked, event.target.checked]);
+    };
+
+    const handleChange2 = (event) => {
+        setChecked([event.target.checked, checked[1]]);
+    };
+
+    const handleChange3 = (event) => {
+        setChecked([checked[0], event.target.checked]);
+    };
+
+    const children = (
+        <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
+            <FormControlLabel
+
+                control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
+            />
+            <FormControlLabel
+
+                control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+            />
+        </Box>
+    );
+
 
     return (
         <>
@@ -22,84 +51,98 @@ const IngredientAlert = () => {
                     </div>
                 </div>
                 <div className="boxIngredient-table">
-                    <table className='min-w-full border border-gray-300'>
-                        <thead className='bg-gray-200'>
-                            <tr>
-                                <th className='px-4 py-2 text-center border-b'>Mã NL</th>
-                                <th className='px-4 py-2 text-center border-b'>Tên NL</th>
-                                <th className='px-4 py-2 text-center border-b'>Danh mục</th>
-                                <th className='px-4 py-2 text-center border-b'>Tồn kho</th>
-                                <th className='px-4 py-2 text-center border-b'>Ngưỡng</th>
-                                <th className='px-4 py-2 text-center border-b'>Đơn vị</th>
-                                <th className='px-4 py-2 text-center border-b'>Tiêu thụ</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className='border border-b-2'>
-                                <td className='px-4 py-2 text-center border-b'>1</td>
-                                <td className='px-4 py-2 text-center border-b'>Gà</td>
-                                <td className='px-4 py-2 text-center border-b'>Thịt</td>
-                                <td className='px-4 py-2 text-center border-b'>10</td>
-                                <td className='px-4 py-2 text-center border-b'>15</td>
-                                <td className='px-4 py-2 text-center border-b'>Con</td>
-                                <td className="px-4 py-2 border-b">
-                                    <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
-                                        <progress
-                                            max={100}
-                                            value={90}
-                                            className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
-                                        ></progress>
-                                        <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
-                                            90%
-                                        </span>
-                                    </div>
-                                </td>
-                            </tr>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={checked[0] && checked[1]}
+                                indeterminate={checked[0] !== checked[1]}
+                                onChange={handleChange1}
+                            />
+                        }>
 
-                            <tr className='border border-b-2'>
-                                <td className='px-4 py-2 text-center border-b'>1</td>
-                                <td className='px-4 py-2 text-center border-b'>Gà</td>
-                                <td className='px-4 py-2 text-center border-b'>Thịt</td>
-                                <td className='px-4 py-2 text-center border-b'>10</td>
-                                <td className='px-4 py-2 text-center border-b'>15</td>
-                                <td className='px-4 py-2 text-center border-b'>Con</td>
-                                <td className="px-4 py-2 border-b">
-                                    <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
-                                        <progress
-                                            max={100}
-                                            value={90}
-                                            className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
-                                        ></progress>
-                                        <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
-                                            90%
-                                        </span>
-                                    </div>
-                                </td>
-                            </tr>
+                        <table className='min-w-full border border-gray-300'>
+                            <thead className='bg-gray-200'>
+                                <tr>
 
-                            <tr className='border border-b-2'>
-                                <td className='px-4 py-2 text-center border-b'>1</td>
-                                <td className='px-4 py-2 text-center border-b'>Gà</td>
-                                <td className='px-4 py-2 text-center border-b'>Thịt</td>
-                                <td className='px-4 py-2 text-center border-b'>10</td>
-                                <td className='px-4 py-2 text-center border-b'>15</td>
-                                <td className='px-4 py-2 text-center border-b'>Con</td>
-                                <td className="px-4 py-2 border-b">
-                                    <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
-                                        <progress
-                                            max={100}
-                                            value={90}
-                                            className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
-                                        ></progress>
-                                        <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
-                                            90%
-                                        </span>
-                                    </div>
-                                </td>
-                            </tr>
+                                    <th className='px-4 py-2 text-center border-b'>Mã NL</th>
+                                    <th className='px-4 py-2 text-center border-b'>Tên NL</th>
+                                    <th className='px-4 py-2 text-center border-b'>Danh mục</th>
+                                    <th className='px-4 py-2 text-center border-b'>Tồn kho</th>
+                                    <th className='px-4 py-2 text-center border-b'>Ngưỡng</th>
+                                    <th className='px-4 py-2 text-center border-b'>Đơn vị</th>
+                                    <th className='px-4 py-2 text-center border-b'>Tiêu thụ</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className='border border-b-2'>
+                                    <td><Checkbox /></td>
+                                    <td className='px-4 py-2 text-center border-b'>1</td>
+                                    <td className='px-4 py-2 text-center border-b'>Gà</td>
+                                    <td className='px-4 py-2 text-center border-b'>Thịt</td>
+                                    <td className='px-4 py-2 text-center border-b'>10</td>
+                                    <td className='px-4 py-2 text-center border-b'>15</td>
+                                    <td className='px-4 py-2 text-center border-b'>Con</td>
+                                    <td className="px-4 py-2 border-b">
+                                        <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
+                                            <progress
+                                                max={100}
+                                                value={90}
+                                                className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
+                                            ></progress>
+                                            <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
+                                                90%
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
 
-                        </tbody>
-                    </table>
+                                <tr className='border border-b-2'>
+                                    <td><Checkbox /></td>
+                                    <td className='px-4 py-2 text-center border-b'>1</td>
+                                    <td className='px-4 py-2 text-center border-b'>Gà</td>
+                                    <td className='px-4 py-2 text-center border-b'>Thịt</td>
+                                    <td className='px-4 py-2 text-center border-b'>10</td>
+                                    <td className='px-4 py-2 text-center border-b'>15</td>
+                                    <td className='px-4 py-2 text-center border-b'>Con</td>
+                                    <td className="px-4 py-2 border-b">
+                                        <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
+                                            <progress
+                                                max={100}
+                                                value={90}
+                                                className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
+                                            ></progress>
+                                            <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
+                                                90%
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr className='border border-b-2'>
+                                    <td><Checkbox /></td>
+                                    <td className='px-4 py-2 text-center border-b'>1</td>
+                                    <td className='px-4 py-2 text-center border-b'>Gà</td>
+                                    <td className='px-4 py-2 text-center border-b'>Thịt</td>
+                                    <td className='px-4 py-2 text-center border-b'>10</td>
+                                    <td className='px-4 py-2 text-center border-b'>15</td>
+                                    <td className='px-4 py-2 text-center border-b'>Con</td>
+                                    <td className="px-4 py-2 border-b">
+                                        <div className="relative w-full max-w-[200px] mx-auto h-[20px]">
+                                            <progress
+                                                max={100}
+                                                value={90}
+                                                className="alert-progress w-full h-full appearance-none [&::-webkit-progress-bar]:bg-gray-200 [&::-webkit-progress-value]:bg-orange-500 rounded"
+                                            ></progress>
+                                            <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">
+                                                90%
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </FormControlLabel>
                 </div>
             </div>
         </>
