e0823890a97ffd62b41d1ad5b06f5a21f263d930
Thanhhao2005 <haovtv12345@gmail.com>
2025-10-25 09:10:02 +0700

    up load chức năng Restaurant Information component

--- pr_qlnh/frontend/src/App.jsx
+++ pr_qlnh/frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OrderPage from "./pages/OrderPage";
 import DishTable from "./components/08_hao-QLMonAn/DishTable";
+import HomePage from "./components/08_hao-QLMonAn/HomePage";
 
 function App() {
   // Vì đã định nghĩa OrderPage ở trên, chúng ta sử dụng nó ở đây
   return (
     <Router>
       <Routes>
+        <Route path="/homeuser" element={<HomePage />} />
         <Route path="/dishtable" element={<DishTable />} />
         <Route path="/order" element={<OrderPage />} />
       </Routes>
