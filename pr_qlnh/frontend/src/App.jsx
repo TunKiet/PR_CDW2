17932072cd889ecd6bc8273ac0f0d34cb9f8e697
Thanhhao2005 <haovtv12345@gmail.com>
2025-10-28 08:42:22 +0700

    up load component restaurant_infor_management

--- pr_qlnh/frontend/src/App.jsx
+++ pr_qlnh/frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import OrderPage from "./pages/OrderPage";
+import Res_Infor_Manage from "./components/08_hao-QLMonAn/restaurant_infor_manage.jsx";
 
 function App() {
   // Vì đã định nghĩa OrderPage ở trên, chúng ta sử dụng nó ở đây
-  return <OrderPage />;
+  return <Res_Infor_Manage />;
 }
 export default App;
