1942f50a9080249d702a928e0f094b7cc8bbddcd
LePhuongLoc <103478293+PhuongLoc@users.noreply.github.com>
2025-10-28 10:24:42 +0700

    fix distable page

--- pr_qlnh/frontend/src/pages/OrderPage.jsx
+++ pr_qlnh/frontend/src/pages/OrderPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/Sidebar/Sidebar";
 import MenuList from "../components/MenuList";
 import OrderSummary from "../components/OrderSummary";
 import TableModal from "../components/TableModal";
